3c4b03cda1b17f27a59e151ca3f05d31d1877245
npiriou <71148909+npiriou@users.noreply.github.com>
2020-11-23 15:47:04 +0100

    uppgrade UI

--- client/src/client.js
+++ client/src/client.js
@@ -20,7 +20,8 @@ const writeName = (name, idx) => {
     const el = document.querySelector(`#name-p${idx} `);
     el.innerHTML = name;
 }
-const addToken =(turn, playerIndex) =>{
+async function addToken(turn, playerIndex) {
+   await dropAnimation(turn, playerIndex);
     const el = document.getElementById(turn);
     el.classList.add(`player${playerIndex}`);
 }
@@ -29,7 +30,7 @@ const askName = () => {
     document.getElementById("myModal").style.display = "block";
 }
 
-const askAgain = (victoryOrDefeat)=>{
+const askAgain = (victoryOrDefeat) => {
     document.getElementById("end-text").innerHTML = victoryOrDefeat;
     document.getElementById("endModal").style.display = "block";
 }
@@ -37,21 +38,21 @@ const askAgain = (victoryOrDefeat)=>{
 const addButtonListeners = () => {
     // mettre les onclic sur les cases
     cases = Array.from(document.getElementsByClassName("case"));
-    cases.forEach((e)=> {
-    e.addEventListener('click', () => {
-        sock.emit('turn', e.id);
+    cases.forEach((e) => {
+        e.addEventListener('click', () => {
+            sock.emit('turn', e.id);
+        });
     });
-});
-// mettre les onclick sur les boutons du menu de fin de partie
-document.getElementById("rematch").addEventListener('click', () => {
-    document.getElementById("rematch").style.display='none';
-    document.getElementById("end-waiting").style.display='block';
-    sock.emit('rematch');
-});
-document.getElementById("new-game").addEventListener('click', ()=>{
-    sock.emit("new-game");
-    window.location.reload();
-});
+    // mettre les onclick sur les boutons du menu de fin de partie
+    document.getElementById("rematch").addEventListener('click', () => {
+        document.getElementById("rematch").style.display = 'none';
+        document.getElementById("end-waiting").style.display = 'block';
+        sock.emit('rematch');
+    });
+    document.getElementById("new-game").addEventListener('click', () => {
+        sock.emit("new-game");
+        window.location.reload();
+    });
 }
 
 const onFormSubmitted = (e) => {
@@ -71,21 +72,44 @@ const onNameSubmitted = (e) => {
     sock.emit('name', text); //on envoie le nom pour le collegue
 };
 
-const forcedReload = ()=>{
-    if(!alert(`The other player don't want to play anymore`)){window.location.reload();}
+const forcedReload = () => {
+    if (!alert(`The other player don't want to play anymore`)) { window.location.reload(); }
 }
 
-const playAgain = () =>{
+const playAgain = () => {
     let cells;
     for (let i = 0; i < 2; i++) {
         cells = Array.from(document.getElementsByClassName(`player${i}`));
         cells.forEach(cell => cell.classList.remove(`player${i}`));
     }
     document.getElementById("endModal").style.display = "none";
-    document.getElementById("end-waiting").display='none';
+    document.getElementById("end-waiting").style.display = 'none';
+    document.getElementById("rematch").style.display = 'flex';
 }
 
-writeEvent("Welcome to RPS");
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function dropAnimation(idCell, idPlayer) { // ex ( "col0-r3", 0 )
+    const cell = [];
+    const turnArr = Array.from(idCell);
+    cell.push(turnArr[6]); //r-x
+    cell.push(turnArr[3]); // col-y
+    // cell est sous la forme [x,y]
+
+    for (let i = 0; i < cell[0]; i++) {
+        document.getElementById(`col${turnArr[3]}-r${i}`).classList
+            .add(`player${idPlayer}`);
+await delay(50);
+        document.getElementById(`col${turnArr[3]}-r${i}`).classList
+            .remove(`player${idPlayer}`);
+    }
+const audio = document.getElementById("sound");
+    audio.play();
+}
+
+writeEvent("Welcome to Connect 4");
 
 const sock = io();
 sock.on('message', writeEvent);
